faf0afe1204465619d161e38a937fe54a73c8f48
haliespaige <halie.romasco@gmail.com>
2023-06-13 10:54:13 -0700

    saving changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many: shacks
+  has_many :shacks
 
 end
